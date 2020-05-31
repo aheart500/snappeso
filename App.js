e360164a526713726b13c1ee81981a741219f038
MohamedNasser <aheart500@yahoo.com>
2020-05-31 16:48:50 +0200

    first

--- App.js
+++ App.js
@@ -24,7 +24,24 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(express.static("frontend"));
-
+app.get("/admin", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
+app.get("/admin-login", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
+app.get("/blog", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
+app.get("/blog/*", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
+app.get("/add-account", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
+app.get("/subscription", (req, res) => {
+  res.sendFile("./frontend/index.html");
+});
 app.use("/api/admin", adminRouter);
 app.use("/api/user", userRouter);
 
